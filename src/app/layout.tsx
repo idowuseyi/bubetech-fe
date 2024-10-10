dbcd621942ad4854adc1c2471fc896c130bf4ad8
geodevcode <geodevinstitute@gmail.com>
2024-10-10 18:08:05 +0100

    background color added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} bg-[#F6F6F6]`}>
         {children}
       </body>
     </html>
