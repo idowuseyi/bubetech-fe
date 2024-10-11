4cb44f34511177c84c0e4d914c576bb93b266b90
sadiq <sabitsdq@gmail.com>
2024-10-11 10:47:41 +0100

    made some changes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { useState } from "react";
-import Post from "../components/home/Post";
 import Connect from "../components/home/connect";
 import WeatherCard from "../components/cards/WeatherCard";
-import CreatePost from "../components/home/createPost";
 import CreatePostDialog from "../components/home/createPostDialog";
 
 export default function Home() {
@@ -13,8 +11,8 @@ export default function Home() {
             <main className="flex flex-col md:flex-row w-full min-w-0 gap-5">
                 <div className="w-full min-w-0 gap-5 flex flex-col">
                     <WeatherCard />
-                    <CreatePost handleShowCreateDialog={() => setShowPostDialog(true)} />
-                    <Post />
+                    {/* <CreatePost handleShowCreateDialog={() => setShowPostDialog(true)} />
+                    <Post /> */}
                 </div>
                 <Connect />
             </main>
