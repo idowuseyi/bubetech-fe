d468777a4b8138e83a5cdb10e64c7379f677419a
sabitsadiq <sabitsdq@gmail.com>
2024-11-11 21:17:07 +0100

    added accept order modal

--- src/app/components/market-components/chat/OrderSummary.tsx
+++ src/app/components/market-components/chat/OrderSummary.tsx
@@ -1,25 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image'
 import tomato from "@/assets/market-place/tomato.jpg";
+import AcceptOffer from '../dialog/AcceptOffer';
 
 export default function OrderSummary() {
+    const [openAcceptOfferDialog, setOpenAcceptOfferDialog] = useState(false)
     return (
-        <div className='bg-[#FFFFFF] w-full flex-1 md:w-[440px] md:flex-[2] rounded-[20px] p-6'>
-            <div className='flex flex-col gap-3 pb-4'>
-                <h4 className='text-base font-medium text-[#323232]'>Order Summary</h4>
-                <div className='flex gap-3'>
-                    <div className='w-20 h-20 rounded-lg'>
-                        <Image src={tomato} alt="product-image" className="w-full h-full object-cover rounded-lg" />
+        <>
+            <div className='bg-[#FFFFFF] w-full flex-1 md:w-[440px] md:flex-[2] rounded-[20px] p-6'>
+                <div className='flex flex-col gap-3 pb-4'>
+                    <h4 className='text-base font-medium text-[#323232]'>Order Summary</h4>
+                    <div className='flex gap-3'>
+                        <div className='w-20 h-20 rounded-lg'>
+                            <Image src={tomato} alt="product-image" className="w-full h-full object-cover rounded-lg" />
+                        </div>
+                        <div className='flex flex-col gap-1 text-[#323232]'>
+                            <h4 className='text-base font-medium'>Tomatoes</h4>
+                            <p className='text-sm text-[#A1A1A1] leading-[18px]'>Size: 5kg</p>
+                            <h4 className='text-base font-medium'>N32,000</h4>
+                        </div>
                     </div>
-                    <div className='flex flex-col gap-1 text-[#323232]'>
-                        <h4 className='text-base font-medium'>Tomatoes</h4>
-                        <p className='text-sm text-[#A1A1A1] leading-[18px]'>Size: 5kg</p>
-                        <h4 className='text-base font-medium'>N32,000</h4>
-                    </div>
+                    <button className='border  border-[#767676] mt-14 rounded-[10px] w-full text-center text-sm text-[#767676] h-12' onClick={() => setOpenAcceptOfferDialog(true)}>Accept Offer</button>
                 </div>
-                <button className='border  border-[#767676] mt-14 rounded-[10px] w-full text-center text-sm text-[#767676] h-12'>Accept Offer</button>
-            </div>
 
-        </div>
+            </div>
+            <AcceptOffer open={openAcceptOfferDialog}
+                onClose={() => setOpenAcceptOfferDialog(false)} />
+        </>
     )
 }
--- src/app/components/market-components/dialog/AcceptOffer.tsx
+++ src/app/components/market-components/dialog/AcceptOffer.tsx
@@ -7,30 +7,32 @@ import {
 import { IoCloseOutline } from 'react-icons/io5';
 import Link from 'next/link';
 
-interface CreateOfferProps {
+interface AcceptOfferProps {
     open: boolean;
     onClose: () => void;
 }
 
-export default function CreateOffer({ open, onClose }: CreateOfferProps) {
+export default function AcceptOffer({ open, onClose }: AcceptOfferProps) {
     return (
         <Dialog open={open}>
-            <DialogContent className='flex flex-col w-11/12 max-w-[350px] rounded-[20px]'>
+            <DialogContent className='flex flex-col w-11/12 max-w-[450px] rounded-[20px]'>
 
                 <div className=' w-full flex items-center justify-between'>
 
-                    <h2 className='text-base font-medium text-[#323232]'>Make an Offer</h2>
+                    <h2 className='text-base font-medium text-[#323232]'>Accept Offer</h2>
                     <div className="bg-[#D9D9D9] w-6 h-6 rounded-full flex items-center justify-center cursor-pointer">
                         <IoCloseOutline size={18} fontWeight={32} onClick={onClose} />
                     </div>
                 </div>
+                <p className='text-[#6FA521] text-xl leading-[30px]'>1 basket of tomatoes</p>
                 <form className='flex flex-col space-y-4'>
                     <div>
                         <input
-                            className="w-full p-3 focus:outline-none cursor-text bg-transparent text-black border-[1.3px] border-[#6C748B] rounded-md"
+                            className="w-full p-3 focus:outline-none cursor-text bg-[#DDDDDD] text-[#9D9D9D] border-none rounded-md"
                             type="text"
-                            placeholder="Enter your bid here"
+                            placeholder="N31 000"
                             maxLength={24}
+                            readOnly
                         />
                     </div>
                     {/* === Submit Button === */}
@@ -39,7 +41,7 @@ export default function CreateOffer({ open, onClose }: CreateOfferProps) {
                             className="bg-[#6FA521] rounded-md mt-3 xl:mt-10 w-full h-12 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm"
                             type="button"
                         >
-                            <span className="text-base font-medium" >Send</span>
+                            <span className="text-base font-medium" >Accept</span>
                         </Button>
                     </Link>
 
@@ -50,4 +52,3 @@ export default function CreateOffer({ open, onClose }: CreateOfferProps) {
         </Dialog>
     )
 }
-
